9b8406dadd096656e9ce3e09c91daa22c1e7e15c
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-29 11:38:19 +0300

    arrow

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-let  divide= function() {
-    return (2000/100);
-  }
-let square=x=x*x;
-let parameter=[1,2,3];  
-let squares = parameter.map(x => x*x);
-return square;
-let add = (parameter1, parameter2) => parameter1 + parameter2;
-add(2,3); 
+let divide = function () {
+	return (2000 / 100);
+	}
+let square = x => x * x;
+let add = (param1, param2) => param1 + param2;
